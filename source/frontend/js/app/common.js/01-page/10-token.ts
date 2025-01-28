65f567bf11315142e220c6accb0feab112fc8565
Zeph LaBoden! <zlaboden@gmail.com>
2025-01-28 10:42:12 -0800

    [eslint] updating linting for frontend common

--- source/frontend/js/app/common.js/01-page/10-token.ts
+++ source/frontend/js/app/common.js/01-page/10-token.ts
@@ -1,17 +1,13 @@
-
 namespace AvsToken {
+  export function main() {
+    AvsFactory.StartPage.Config.create({
+      verificationVersion: Application.verificationVersion,
+      partnerId: Application.p,
+      payload: Application.d,
+      sessionId: Application.sessionId,
+      partnerColorConfig: Application.partnerColorConfig,
+    });
 
-	export function main() {
-
-		AvsFactory.StartPage.Config.create({
-			verificationVersion           : Application.verificationVersion,
-			partnerId                     : Application.p,
-			payload                       : Application.d,
-			sessionId                     : Application.sessionId,
-			partnerColorConfig            : Application.partnerColorConfig,
-		});
-
-		AvsFactory.StartPage.init();
-	}
-
+    AvsFactory.StartPage.init();
+  }
 }
